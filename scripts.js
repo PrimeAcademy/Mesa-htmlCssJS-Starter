dcb4f7b47d42296ca62de7de47db01695892c021
Alex Bliss <alexander.blss@gmail.com>
2024-07-31 19:17:42 -0500

    midway point

---
+++ scripts.js
@@ -0,0 +1,43 @@
+// numbers 18
+// strings ' or " "taco"
+// boolean true or false
+
+//let       x          =          1
+          //Name    Assigment    value
+//let y = "taco"
+
+let z = true
+
+
+
+let twoCats = "zack cody"
+
+
+let x = 1
+
+x = x + 2
+// x = 3
+x += 2
+// x = 5
+x++
+//++ means add 1
+// x = 6
+
+let y = "1"
+    y         =          y + 2
+// y = "1"    =         "1"      +       2
+//we want 3
+// we get '12'
+
+let d = "dog"
+let c = "cat"
+//c + d = "catdog"
+let myHouse = c + " " + d 
+//myHouse = cat dog
+let n = null
+
+null
+NaN
+undefined
+
+const MNSalesTax = 7.625
