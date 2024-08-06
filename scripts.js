988bf4fd3f5050ad2289f6b80efab983091ba2c5
Alex Bliss <alexander.blss@gmail.com>
2024-08-06 18:40:19 -0500

    end of Array lecture

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+console.log("JS working")
 // numbers 18
 // strings ' or " "taco"
 // boolean true or false
@@ -8,10 +9,7 @@
 
 let z = true
 
-
-
 let twoCats = "zack cody"
-
 
 let x = 1
 
@@ -35,9 +33,66 @@ let c = "cat"
 let myHouse = c + " " + d 
 //myHouse = cat dog
 let n = null
-
+//console.log(myHouse)
 null
 NaN
 undefined
 
 const MNSalesTax = 7.625
+y = 6
+//console.log(y , 'before if')
+if(y != "1"  || x == 6){
+//console.log(y , 'yay')
+} 
+else if(y === "12"){
+// console.log(
+// "number = 12"
+// );
+}
+else {
+    // console.log('nope')
+}
+
+//  . - dot
+// () - parens
+// [] - brackets, square braces
+// $ - bling, cash, dollar sign
+// {} - curlies,curly brackets, curly braces
+//> < - aligators, greater than, less than, bird, carrot
+// \ back slash
+// / slash
+// ! - not, Bang
+// # - pound , hashtag
+// * - star, splat
+// || - or, pipes
+// NaN - not a number, bread
+
+let theSimpsons = ['homer' , 'marge', 'bart', 'lisa', 'magie']
+
+let exampleArray = ['yes', 23 , true ]
+
+let ninthItem = theSimpsons[9]
+let firstItem = theSimpsons[0]
+console.log(theSimpsons[1])
+console.log(theSimpsons[2])
+console.log(theSimpsons[3])
+console.log(theSimpsons[9])
+console.log(ninthItem)
+
+console.log(theSimpsons.length);
+let endOfArray = theSimpsons.length - 1;
+console.log(endOfArray);
+let lastItem = theSimpsons[theSimpsons.length - 1];
+// let lastItem = theSimpsons.length[4];
+console.log(lastItem);
+
+console.log('changeing Arrays')
+
+theSimpsons.push('Abe')
+console.log(theSimpsons);
+theSimpsons.pop()
+console.log(theSimpsons);
+theSimpsons.unshift('abe')
+console.log(theSimpsons);
+theSimpsons.shift()
+console.log(theSimpsons);
